79c223909d69e17abfbe86b2f82990867c7bd777
Michael Ragusa <mragusa@users.noreply.github.com>
2025-07-02 14:23:10 -0400

    added ipv6 support, removed duplicate code and fixed errors in lambda functions

--- find-dns-server.py
+++ find-dns-server.py
@@ -8,7 +8,7 @@ import logging
 
 logging.getLogger("scapy").setLevel(logging.CRITICAL)
 
-from scapy.all import rdpcap, DNS
+from scapy.all import rdpcap, IP, IPv6, UDP, TCP
 
 dns_servers_found = {}
 recursive_dns_servers = {}
@@ -16,29 +16,51 @@ dns_clients = {}
 
 
 def find_dns_servers(packet):
-    if "UDP" in packet:
-        if packet["UDP"].dport == 53:
-            if packet["IP"].dst in dns_servers_found:
-                dns_servers_found[packet["IP"].dst] += 1
+    if UDP in packet and packet[UDP].dport == 53:
+        if IP in packet:
+            if packet[IP].dst in dns_servers_found:
+                dns_servers_found[packet[IP].dst] += 1
             else:
-                dns_servers_found[packet["IP"].dst] = 1
-        if packet["UDP"].sport == 53:
-            if packet["IP"].dst in dns_clients:
-                dns_clients[packet["IP"].dst] += 1
+                dns_servers_found[packet[IP].dst] = 1
+        if IPv6 in packet:
+            if packet[IPv6].dst in dns_servers_found:
+                dns_servers_found[packet[IPv6].dst] += 1
             else:
-                dns_clients[packet["IP"].dst] = 1
+                dns_servers_found[packet[IPv6].dst] = 1
+    if UDP in packet and packet[UDP].sport == 53:
+        if IP in packet:
+            if packet[IP].dst in dns_clients:
+                dns_clients[packet[IP].dst] += 1
+            else:
+                dns_clients[packet[IP].dst] = 1
+        if IPv6 in packet:
+            if packet[IPv6].dst in dns_clients:
+                dns_clients[packet[IPv6].dst] += 1
+            else:
+                dns_clients[packet[IPv6].dst] = 1
 
-    if "TCP" in packet:
-        if packet["TCP"].dport == 53:
-            if packet["IP"].dst in dns_servers_found:
-                dns_servers_found[packet["IP"].dst] += 1
+    if TCP in packet and packet[TCP].dport == 53:
+        if IP in packet:
+            if packet[IP].dst in dns_servers_found:
+                dns_servers_found[packet[IP].dst] += 1
             else:
-                dns_servers_found[packet["IP"].dst] = 1
-        if packet["TCP"].sport == 53:
-            if packet["IP"].dst in dns_clients:
-                dns_clients[packet["IP"].dst] += 1
+                dns_servers_found[packet[IP].dst] = 1
+        if IPv6 in packet:
+            if packet[IPv6].dst in dns_servers_found:
+                dns_servers_found[packet[IPv6].dst] += 1
             else:
-                dns_clients[packet["IP"].dst] = 1
+                dns_servers_found[packet[IPv6].dst] = 1
+    if TCP in packet and packet[TCP].sport == 53:
+        if IP in packet:
+            if packet[IP].dst in dns_clients:
+                dns_clients[packet[IP].dst] += 1
+            else:
+                dns_clients[packet[IP].dst] = 1
+        if IPv6 in packet:
+            if packet[IPv6].dst in dns_clients:
+                dns_clients[packet[IPv6].dst] += 1
+            else:
+                dns_clients[packet[IPv6].dst] = 1
 
 
 def main(file, display, count, focus):
@@ -73,14 +95,14 @@ def main(file, display, count, focus):
                 sorted_recursive_servers = dict(
                     sorted(
                         recursive_dns_servers.items(),
-                        key=lambda item: [1],
+                        key=lambda item: item[1],
                         reverse=True,
                     )
                 )
                 type_choice["recursive"] = sorted_recursive_servers
             if focus == "clients":
                 sorted_clients = dict(
-                    sorted(dns_clients.items(), key=lambda item: [1], reverse=True)
+                    sorted(dns_clients.items(), key=lambda item: item[1], reverse=True)
                 )
                 type_choice["clients"] = sorted_clients
             if count:
@@ -98,28 +120,13 @@ def main(file, display, count, focus):
                         else:
                             c += 1
             else:
-                if focus == "servers":
-                    sorted_data = dict(
-                        sorted(
-                            dns_servers_found.items(),
-                            key=lambda item: item[1],
-                            reverse=True,
+                if type_choice:
+                    for n in type_choice[focus]:
+                        print(
+                            "{}: {} Count: {}".format(focus, n, type_choice[focus][n])
                         )
-                    )
-                if focus == "recursive":
-                    sorted_data = dict(
-                        sorted(
-                            recursive_dns_servers.items(),
-                            key=lambda item: [1],
-                            reverse=True,
-                        )
-                    )
-                if focus == "clients":
-                    sorted_data = dict(
-                        sorted(dns_clients.items(), key=lambda item: [1], reverse=True)
-                    )
-                for n in sorted_data:
-                    print("{}: {} Count: {}".format(focus, n, sorted_data[n]))
+                else:
+                    print("No sorted data found")
         else:
             print("Total DNS servers found: {}".format(len(dns_servers_found)))
             print(
